c4de89abf327543d9f79f8dd316a561119614b54
ihnatsen <ivanihnatsenkau030202@gmail.com>
2026-03-22 19:26:19 +0100

    init

---
+++ calculater.py
@@ -0,0 +1,8 @@
+def my_sum(a, b):
+    return a + b
+
+def my_sub(a, b):
+    return a - b
+
+def my_mul(a, b):
+    return a * b
---
+++ test_calculater.py
@@ -0,0 +1,5 @@
+import pytest
+from calculater import *
+
+def test_add():
+    assert 2 == my_sum(1, 1)
